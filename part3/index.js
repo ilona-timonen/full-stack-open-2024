18edb5577ff8c9ebf480c366d6dca9dddb3e4e09
Ilona Timonen <ilona.s.timonen@tuni.fi>
2024-06-10 17:24:30 +0300

    index

--- part3/index.js
+++ part3/index.js
@@ -97,7 +97,7 @@ app.use((request, response) => {
   response.status(404).send({ error: 'Unknown endpoint' });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001 || 3002;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
